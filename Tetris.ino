540e1f15e4de3d6b5fb5819d3222208e15a88958
Ruud van Falier <ruud@partechit.nl>
2017-01-12 13:20:54 +0100

    Revised the way bits are set and ordered (untested)

--- Tetris.ino
+++ Tetris.ino
@@ -1,9 +1,23 @@
+//#include <avr/io.h>
+
 #define SR_LATCH 2
 #define SR_CLOCK 3
 #define SR_DATA 4
 #define SR_RESET 5
 
-byte colors[][3] = {
+/*
+#define SHIFT_REGISTER DDRB
+#define SHIFT_PORT PORTB
+#define DATA (1<<PB3)    //MOSI (SI)
+#define LATCH (1<<PB2)    //SS   (RCK)
+#define CLOCK (1<<PB5)    //SCK  (SCK)
+
+// http://jumptuck.com/2011/11/03/how-to-drive-595-shift-registers-with-avr-hardware-spi/
+*/
+
+// Color data.
+// Always on = 10, always off = 0, otherwise on for number of cycles.
+byte palette[][3] = {
   {  0,  0,  0 },
   { 10,  0,  0 },
   {  0, 10,  0 },
@@ -14,6 +28,7 @@ byte colors[][3] = {
   { 10, 10, 10 }
 };
 
+// The frame buffer (pixel data).
 byte framebuffer[20][10] = {
   { 0, 0, 0, 0, 0, 0, 0, 0, 7, 7 },
   { 0, 1, 2, 3, 4, 5, 6, 0, 7, 7 },
@@ -62,7 +77,7 @@ byte framebuffer[20][10] = {
 };
 */
 
-byte data[7]; // SR data
+byte data[7]; // Shift register data
 
 void setup() 
 {
@@ -76,76 +91,71 @@ void setup()
   digitalWrite(SR_DATA, LOW);
   digitalWrite(SR_RESET, HIGH);
 
-  //Serial.begin(9600);
-  //Serial.println("Setup done");
+  /*
+  SHIFT_REGISTER |= (DATA | LATCH | CLOCK);  //Set control pins as outputs
+  SHIFT_PORT &= ~(DATA | LATCH | CLOCK);    //Set control pins low
+
+  SPCR = (1<<SPE) | (1<<MSTR);  //Start SPI as Master
+  */
+  
+  Serial.begin(9600);
+  Serial.println("Setup done");
 }
 
 void loop() 
 {
+  unsigned long start_time = micros();
+  
   for (int row = 0; row < 20; row++)
   {
-    //Serial.print("Row ");
-    //Serial.println(row);
-    
-    data[0] = 0;
-    data[1] = 0;
-    data[2] = 0;
+    unsigned long row_data = 0; // 32 bits that are all disabled.
+                                // The first 20 bits of row_data are used for the state of the row outputs (top to bottom).
 
-    int index = 0;
-    int shift = 0;
-
-    if (row < 8)
-      index = 0;
-    else if (row < 16)
-      index = 1;
-    else
-      index = 2;
-
-    if (row > 0)
-    {
-      shift = row - (8 * (row / 8));
-    }
-
-    // set row bit
-    data[index] = 1 << shift;
+    row_data |= 1 << (32 - row - 1);  // Set enabled bit for current row
+                                      // Example: if row == 2 (which is the 3rd row) then row_data = 00100000000000000000000000000000
     
     for (int col = 0; col < 10; col++)
     {
       if (framebuffer[row][col] == 0)
       {
-        // display nothing
-        continue;
+        clear_sr(); // Clear all the outputs.
+        continue;   // Continue processing next column.
       }
 
-      uint32_t column_data2 = 4294967295;
-
-      // Get RGB value for current pixel in framebuffer
-      byte r = colors[framebuffer[row][col]][0];
-      byte g = colors[framebuffer[row][col]][1];
-      byte b = colors[framebuffer[row][col]][2];
+      uint32_t column_data = (uint32_t)-1;  // 32 bits that are all enabled.
+                                            // The first 30 bits of column_data are used for the state of the column outputs (left to right).
+                                            // 0 - 9 = RED, 10 - 19 = GREEN, 20 - 29 = BLUE.
+      
+      // Get RGB value for current pixel in framebuffer.
+      byte r = palette[framebuffer[row][col]][0];
+      byte g = palette[framebuffer[row][col]][1];
+      byte b = palette[framebuffer[row][col]][2];
 
       if (r == 10)
       {
-        column_data2 &= ~(1UL << (col + 20 + 2));
+        column_data &= ~(1UL << (32 - col - 1));
       }
 
       if (g == 10)
       {
-        column_data2 &= ~(1UL << (col + 10 + 2));
+        column_data &= ~(1UL << (32 - col - 10 - 1));
       }
 
       if (b == 10)
       {
-        column_data2 &= ~(1UL << (col + 2));
+        column_data &= ~(1UL << (32 - col - 20 - 1));
       }
 
-      data[3] = (byte)((column_data2 >> 24) & 0xFF) ;
-      data[4] = (byte)((column_data2 >> 16) & 0xFF) ;
-      data[5] = (byte)((column_data2 >> 8) & 0XFF);
-      data[6] = (byte)((column_data2 & 0XFF));
+      // Split row_data and column_data to seperate bytes.
+      data[0] = (byte)((row_data >> 24) & 0xFF);
+      data[1] = (byte)((row_data >> 16) & 0xFF);
+      data[2] = (byte)((row_data >> 8) & 0xFF);
+      data[3] = (byte)((column_data >> 24) & 0xFF);
+      data[4] = (byte)((column_data >> 16) & 0xFF);
+      data[5] = (byte)((column_data >> 8) & 0xFF);
+      data[6] = (byte)(column_data & 0xFF);
 
-/*
-      Serial.print("ROW_");
+      // Output debug data to serial monitor.
       Serial.print(row);
       Serial.print(":\t");
       
@@ -158,120 +168,54 @@ void loop()
         Serial.print("\t");
       }
 
-      //print_bin(column_data2);
-      Serial.print("\t");
-      Serial.print(column_data2);
-
-      //for (int i = 0; i < 30; i++)
-      //{
-      //  Serial.print(column_data[i]);
-      //}
-
       Serial.print("\r\n");
-      //Serial.println(column_data);
-      */
       
-      // shift out data BACKWARDS! (data[7] to data[0])
-      // ...
-
-      digitalWrite(SR_LATCH, LOW);
-
-      for (int i = 7; i >= 0; i--)
-      {
-        // Serial.println(i);
-
-        if (i > 2)
-        {
-          shiftOut(SR_DATA, SR_CLOCK, LSBFIRST, data[i]);
-        }
-        else
-        {
-          shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, data[i]);
-        }
-      }
-      
-      digitalWrite(SR_LATCH, HIGH);
+      // Shift out the data to the shift registers.
+      // The first byte needs to be shift out as last (rows are at the top of the shift registers).
+      // The last bit if each byte needs to be shift out first (LSBFIRST).
+      shiftout_sr(data);
     }
   }
 
-  /*
-  for (int cycle = 0; cycle < 10; cycle++)
-  {
-    for (int i = 0; i < 20; i++)
-    {
-      byte data = 0;
-      byte shift = 0;
-      
-      if (i > 0)
-      {
-        shift = i - (8 * (i / 8));
-      }
-      
-      data = 1 << shift;
+  Serial.print("Duration: ");
+  Serial.println(micros() - start_time);
 
-      // Test PWM
-      if (cycle > 3)
-      {
-        if (i == 4 || i == 8 || i == 12)
-        {
-          data = 0;
-        }
-      }
-
-      if (cycle > 6)
-      {
-        if (i == 6 || i == 10 || i == 14)
-        {
-          data = 0;
-        }
-      }
-  
-      digitalWrite(SR_LATCH, LOW);
-  
-      shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, data);
-  
-      if (i > 0)
-      {
-        // Add empty bits in upper SR's
-        if (i / 8 == 1)
-        {
-          shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, 0);
-        }
-        
-        if (i / 8 == 2)
-        {
-          shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, 0);
-          shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, 0);
-        }
-      }
-      
-      //Serial.print(shift);
-      //Serial.print("\t");
-      //Serial.println(data);
-  
-      digitalWrite(SR_LATCH, HIGH);
-  
-      // Reset
-      digitalWrite(SR_LATCH, LOW);
-      shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, 0);
-      shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, 0);
-      shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, 0);
-      digitalWrite(SR_LATCH, HIGH);
-  
-      //delay(300);
-    }
-  }
-  */
-  
-  //digitalWrite(SR_LATCH, LOW);
-  //shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, B00000000);
-  //shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, B00000000);
-  //shiftOut(SR_DATA, SR_CLOCK, MSBFIRST, B00000000);
-  //digitalWrite(SR_LATCH, HIGH);
-
-  //delay(5000);
+  delay(100000);
 }
 
+// Clear the shift registers.
+void clear_sr()
+{
+  digitalWrite(SR_RESET, LOW);
+  digitalWrite(SR_RESET, HIGH);
+}
+
+// Shift out data to shift register (including latch).
+void shiftout_sr(byte* d)
+{
+  //SHIFT_PORT &= ~LATCH;
+  digitalWrite(SR_LATCH, LOW);
+
+  for (int i = 6; i >= 0; i--)
+  {
+    shiftOut(SR_DATA, SR_CLOCK, LSBFIRST, d[i]);
+    //spi_send(d[i]);
+  }
+  
+  digitalWrite(SR_LATCH, HIGH);
+  //SHIFT_PORT |= LATCH;
+  //SHIFT_PORT &= ~LATCH;
+}
+
+void spi_send(byte d)
+{
+  SPDR = d; //Shift out the data.
+  
+  //while(!(SPSR & (1<<SPIF))); //Wait for SPI process to finish
+  loop_until_bit_is_set(SPSR, SPIF); 
+}
+
+// Serial print binary value of a byte.
 void print_bin(byte val)
 {
     if(val!=0) print_bin(val/2);
